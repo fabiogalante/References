410d190ecf9d7548939d481b23fec2aaca565938
Galante <fabiogalantemans@gmail.com>
2020-02-18 23:02:27 -0300

    PassagemPorValor - PassagemPorReferencia

--- References_Fixed/Program.cs
+++ References_Fixed/Program.cs
@@ -15,8 +15,45 @@ namespace References_Fixed
             {
                 Console.WriteLine(ninja);
             }
+
+            int n = 5;
+
+            PassagemPorValor(n);
+            Console.WriteLine($"Passagem de parâmetros por valor: {n}");
+
+            PassagemPorReferencia(ref n);
+
+            Console.WriteLine($"Passagem de parâmetros por referência: {n}");
+
+
         }
+
+
+        //O ref é para passar o argumento por referência, ou seja, em vez de passar o seu valor,
+        //passa-se um apontador que indica onde está o valor.Desta forma quando o parâmetro tiver
+        //alterado seu valor, estará alterando o valor do argumento já que eles estão na
+        //mesma posição de memória.
         static void AddGreeting(ref string ninja)
-            =>  ninja = "Greetings " + ninja ;
+        {
+            ninja = "Greetings " + ninja;
+        }
+
+
+
+        //O valor é recebido, é alocado um outro espaço na memória que recebe o valor, porém quando
+        //termina o método este espaço na memério é zerado e é impresso o valor 5
+        static void PassagemPorValor(int a)
+        {
+            a = 10;
+        }
+
+
+        //O valor int a é recebido por referência, ou seja é alocado somente um espaço de memória
+        //no momento que a = 10 neste mesmo espaço de memória ele é substituído pelo novo valor
+        //desta forma ele é modificado.
+        static void PassagemPorReferencia(ref int a)
+        {
+            a = 10;
+        }
     }
 }
